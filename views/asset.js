66ea95299f72cba894c01343eac829903db9c773
Daniel Kim <bjkim0526@gmail.com>
2017-03-04 12:09:46 -0800

    changed circle transitions and added some forms

--- views/asset.js
+++ views/asset.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     	trigger: 'manual'
 	});
 	$("#startBtn").click(function(){
-		console.log("whatip");		
+		
 		$(".circle").flip(true);
 	});
 
@@ -13,15 +13,26 @@ $(document).ready(function(){
 	    	$(".front").empty();
 	    	$(".circle").flip(false);
 	    }
-	    $.ajax({
-	      type: "GET",
-	      url: 'http://localhost:3000/api/fit/authorize',
-	      success: function(data){
-	      	$(".front").append("<p id='textForm' style='position: absolute; margin-top:130px; margin-left:100px; font-size:38px;'>Connect Your Fitbit</p><button id='nextBtn' style='margin-top:280px; margin-left: 150px; padding: 13px; font-size:large; font-weight:bold;'><a href = '" + data + "'>Login</a></button>");
-	      }
-	    });
+	    
+	    // $.ajax({
+	    //   type: "GET",
+	    //   url: 'http://localhost:3000/api/fit/authorize',
+	    //   success: function(data){
+	    //   	$(".front").append("<p id='textForm' style='position: absolute; margin-top:130px; margin-left:100px; font-size:38px;'>Connect Your Fitbit</p><button id='nextBtn' style='margin-top:280px; margin-left: 150px; padding: 13px; font-size:large; font-weight:bold;'><a href = '" + data + "'>Login</a></button>");
+	    //   }
+	    // });
+
+
+
+
 	    
 
+	});
+
+	$(".Login").click(function(){
+		$(".front").empty();
+		$(".circle").flip(false);
+		$(".front").append("<div id='circleForm'><p id='login'>Login</p><div class='fields'><div class='group'><input id='nameField' type='text' required><span class='highlight'></span><span class='bar'></span><label>Username</label></div><div class='group'><input id='pwField' type='password' required><span class='highlight'></span><span class='bar'></span><label>Password</label></div></div><button id='loginbtn'>Login</button></div>");
 	});
 
 	
