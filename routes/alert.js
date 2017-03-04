3bd41ac5b566cbd290f32b4fe844cbab0bbb41f5
Shivam Sharma <sdsharma@ucsd.edu>
2017-03-04 11:11:55 -0800

    singulert

--- routes/alert.js
+++ routes/alert.js
@@ -1,13 +1,11 @@
 'use strict';
 
 const Joi = require('joi');
-var FitbitApiClient = require("fitbit-node");
 var MongoClient = require('mongodb').MongoClient
   , assert = require('assert');
-var dburl = 'mongodb://localhost:27017/singularitydiabetes';
+var dburl = 'mongodb://localhost:27017/singulert';
 
-var client = new FitbitApiClient("227H7Z", "d9a1cd45ceb53e96d4b9eeac77af5c60");
-const API_BASE_PATH = '/api/fit';
+const API_BASE_PATH = '/api/alert';
 
 const routes = [];
 function currentDate(){
@@ -48,30 +46,12 @@ routes.push({
     config: {
         auth: false,
         handler: function (request, reply) {
-            reply(client.getAuthorizeUrl('activity heartrate location nutrition profile settings sleep social weight', 'http://localhost:3000/api/fit/callback'));
+            reply(true);
         },
         tags: ['api']
     }
 });
 
-routes.push({
-    method: 'GET',
-    path: API_BASE_PATH + '/callback',
-    config: {
-        auth: false,
-        handler: function (request, reply) {
-            client.getAccessToken(request.query.code, 'http://localhost:3000/api/fit/callback').then(function (result) {
-                var date = currentDate();
-                client.get("/activities/date/" + date + ".json", result.access_token).then(function (results) {
-                    reply(results[0]);
-                });
-            }).catch(function (error) {
-                reply(false);
-            });
-        },
-        tags: ['api']
-    }
-});
 
 routes.push({
     method: 'POST',
