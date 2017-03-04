f72f3a2e12aeb413715311b1dc72df49d06e1e62
Shivam Sharma <sdsharma@ucsd.edu>
2017-03-04 15:22:57 -0800

    initial userprofile page

--- views/asset.js
+++ views/asset.js
@@ -12,20 +12,6 @@ $(document).ready(function(){
 	    	$(".front").empty();
 	    	$(".circle").flip(false);
 	    }
-	    
-	    // $.ajax({
-	    //   type: "GET",
-	    //   url: 'http://localhost:3000/api/fit/authorize',
-	    //   success: function(data){
-	    //   	$(".front").append("<p id='textForm' style='position: absolute; margin-top:130px; margin-left:100px; font-size:38px;'>Connect Your Fitbit</p><button id='nextBtn' style='margin-top:280px; margin-left: 150px; padding: 13px; font-size:large; font-weight:bold;'><a href = '" + data + "'>Login</a></button>");
-	    //   }
-	    // });
-
-
-
-
-	    
-
 	});
 
 	$(".Login").click(function(){
